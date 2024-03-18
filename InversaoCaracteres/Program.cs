88aed42a4352a450b692f1d2d964bee28386b8dd
Camila Rocha <camila.rocha.oliva@hotmail.com>
2024-03-18 10:37:27 -0300

    primeiro commit

---
+++ InversaoCaracteres/Program.cs
@@ -0,0 +1,26 @@
+﻿string frase;
+do
+{
+    Console.Write("Digite uma frase ou palavra para recebe-lá invertida: ");
+    frase = Console.ReadLine()!;
+    List<char> fraseSeparada = frase.ToCharArray().ToList();
+    List<char> fraseSeparadaCopia = new List<char>();
+    int contador = fraseSeparada.Count - 1;
+
+    for (int i = 0; i < fraseSeparada.Count; i++)
+    {
+        fraseSeparadaCopia.Add(fraseSeparada[contador]);
+        contador--;
+    }
+
+    foreach (char c in fraseSeparadaCopia)
+    {
+        Console.Write(c);
+    }
+
+    if (frase == string.Empty)
+    {
+        Console.WriteLine("Precisa informar uma frase ou palavra para ser invertida");
+    }
+}
+while (frase == string.Empty);
---
+++ SequenciaFibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿int primeiroNumero = 0, segundoNumero = 1, numeroAtual = 0;
+
+Console.Write("Informe um número para saber se faz parte da sequência Fibonacci: ");
+string entrada = Console.ReadLine()!;
+
+if (int.TryParse(entrada, out int entradaConvertida))
+{
+    if (entradaConvertida == 0 || entradaConvertida == 1)
+    {
+        Console.WriteLine($"\nO Número {entrada} pertence a sequência de Fibonacci");
+    }
+
+    if (entradaConvertida > 1)
+    {
+        for (int i = 0; i <= entradaConvertida; i++)
+        {
+            i = primeiroNumero + segundoNumero;
+            primeiroNumero = segundoNumero;
+            segundoNumero = i;
+            numeroAtual = i;
+
+            if (i == entradaConvertida)
+            {
+                Console.WriteLine($"\nO Número {entrada} pertence a sequência de Fibonacci");
+            }
+        }
+
+        if (numeroAtual != entradaConvertida)
+        {
+            Console.WriteLine($"\nO Número {entrada} não pertence a sequência de Fibonacci");
+        }
+    }
+}
+
+else
+{
+    Console.WriteLine("\nValor inválido");
+}
